db31a7bcabcb9c3eb332c04961fc9e00b16bb739
mamun2519 <juborajislam46@gmail.com>
2024-07-07 23:33:39 +0600

    add to add file

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.get("/health", (_req, res) => {
   });
 });
 
+app.get("api/v1/articles", (req, res) => {
+  res.status(200).json({ path: "/articles" });
+});
 app.listen(4000, () => {
   console.log("server is listerningon port 4000");
 });
